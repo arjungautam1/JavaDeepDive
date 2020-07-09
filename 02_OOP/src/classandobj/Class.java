7c12ac96aef8ec551b61a64d173295b972ac20b9
arjun-sudo <laserarjun@gmail.com>
2020-07-09 23:27:44 +0545

    class and object

---
+++ 02_OOP/src/classandobj/Class.java
@@ -0,0 +1,14 @@
+package classandobj;
+
+public class Class {
+    int id;
+    String address;
+    Double percentage;
+    void printDetail()
+    {
+        System.out.println("Name is :"+id);
+        System.out.println("Percentage is :"+percentage);
+        System.out.println("Address is :"+address);
+    }
+}
+
---
+++ 02_OOP/src/classandobj/ObjectDemo.java
@@ -0,0 +1,14 @@
+package classandobj;
+
+import classandobj.Class;
+
+public class ObjectDemo {
+    public static void main(String[] args) {
+        Class obj=new Class();
+        obj.id=1;
+        obj.address="Kathmandu";
+        obj.percentage=50.5;
+        obj.printDetail();
+
+    }
+}
