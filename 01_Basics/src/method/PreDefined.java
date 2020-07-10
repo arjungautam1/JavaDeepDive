6de89f2b7588a4da25c486f66d59afae720d88bf
arjun-sudo <laserarjun@gmail.com>
2020-07-10 23:50:30 +0545

    pre defined methods

---
+++ 01_Basics/src/method/PreDefined.java
@@ -0,0 +1,10 @@
+package method;
+
+// Some pre-defined methods are length(), equals(), compareTo(), sqrt(), etc.
+public class PreDefined {
+    public static void main(String[] args) {
+
+        System.out.println("The maximum number is :"+Math.max(9,7));
+
+    }
+}
