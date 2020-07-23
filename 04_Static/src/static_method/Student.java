4958bfabf76ac940aadd4a99a6e3fa37254aa0c0
arjun-sudo <laserarjun@gmail.com>
2020-07-23 20:24:31 +0545

    Static Method

--- 04_Static/src/static_method/Student.java
+++ 04_Static/src/static_method/Student.java
@@ -1,4 +1,32 @@
 package static_method;
 
-public class Student {
+class Student {
+    private int rollNo; //instance variable
+    private String name;
+    private static String college = "Texas International College";
+
+//    This is static method
+
+    static void change() {
+        college = "Texas";
+    }
+
+//    Contructor
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+//    Method to display the values
+
+    void display() {
+        System.out.println(rollNo + " " + name + " " + college);
+    }
+
+//    to hide data using private
+
+    public static void setCollege(String college) {
+        Student.college = college;
+    }
 }
--- 04_Static/src/static_method/TestStaticMethod.java
+++ 04_Static/src/static_method/TestStaticMethod.java
@@ -1,4 +1,14 @@
 package static_method;
 
 public class TestStaticMethod {
+    public static void main(String[] args) {
+        Student s1=new Student(111,"Ram ");
+        Student s2=new Student(112,"Pooja");
+
+        Student.change();
+
+        s1.display();
+        s2.display();
+    }
+
 }
