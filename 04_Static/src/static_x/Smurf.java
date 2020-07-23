8861a908c7d15ba414c71c361067c391ad060b8b
arjun-sudo <laserarjun@gmail.com>
2020-07-23 21:37:04 +0545

    Smurf challenge solved

---
+++ 04_Static/src/static_x/Smurf.java
@@ -0,0 +1,16 @@
+package static_x;
+
+public class Smurf {
+
+    private static String name;
+
+
+    public static void setName(String name) {
+        Smurf.name = name;
+    }
+
+
+    public static String getName() {
+        return name;
+    }
+}
---
+++ 04_Static/src/static_x/SmurfFactory.java
@@ -0,0 +1,14 @@
+package static_x;
+
+public class SmurfFactory {
+    public static void main(String[] args) {
+
+        Smurf.setName("Papa");
+        Smurf.setName("Papa");
+        Smurf.setName("Pugu");
+
+        System.out.println("Creating :"+Smurf.getName()+"Smurff");
+
+
+    }
+}
