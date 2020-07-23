25f479cef3c73b5ee368e5376c69c1f79b8571b3
arjun-sudo <laserarjun@gmail.com>
2020-07-24 00:41:13 +0545

    heirarchical Inheritance

--- 02_OOP/src/inheritance/heirarchical/HeirarchicalInheritance.java
+++ 02_OOP/src/inheritance/heirarchical/HeirarchicalInheritance.java
@@ -1,4 +1,31 @@
 package inheritance.heirarchical;
 
+class Animal {
+    void eat() {
+        System.out.println("Eating ");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Meowing ");
+    }
+}
+
 public class HeirarchicalInheritance {
+
+    public static void main(String[] args) {
+        Cat cat=new Cat();
+    cat.meow();
+//    cat.bark(); // Gives compile time error
+        cat.eat();
+    }
+
+
 }
