97ae599ed699c26c24241c8065a51872ffa25894
arjun-sudo <laserarjun@gmail.com>
2020-07-24 21:00:58 +0545

    abstract class

---
+++ 05_Abstract/src/abstraction/TestBank.java
@@ -0,0 +1,30 @@
+package abstraction;
+
+abstract class Bank{
+    abstract int getRateOfInterest();
+}
+class NMB extends Bank{
+    int getRateOfInterest()
+    {
+        return 7;
+    }
+}
+class NIC extends Bank{
+
+    int getRateOfInterest() {
+        return 8;
+    }
+}
+
+public class TestBank {
+    public static void main(String[] args) {
+        Bank bank;
+        bank = new NMB() ;
+        Bank bank1;
+        bank1=new NIC();
+
+        System.out.println("Rate of Interest is :"+bank.getRateOfInterest());
+        System.out.println("Rate of Interest of abstraction.NIC :"+bank1.getRateOfInterest());
+    }
+
+}
