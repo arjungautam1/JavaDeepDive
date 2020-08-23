e507c6cd43098bc684c6a45b19c7c83cc47aef7d
arjun-sudo <laserarjun@gmail.com>
2020-08-23 18:21:14 +0545

    java

---
+++ 06_compare_strings/compare_strings/StringsRFun.java
@@ -0,0 +1,20 @@
+package compare_strings;
+
+public class StringsRFun {
+
+	public static void main(String[] args) {
+		
+		String a=new String("arjun");
+		String b=new String("arjun");
+		
+		if(a.equals(b)) {
+			System.out.println("true");
+		
+		}
+		else {
+			System.out.println("false");
+		}
+
+	}
+
+}
